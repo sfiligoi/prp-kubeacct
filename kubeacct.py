0ec4230415593d55f5c0282570687ee3876ce2ac
naamabareket <52012489+naamabareket@users.noreply.github.com>
2019-06-27 11:44:14 -0700

    Add files via upload

--- kubeacct.py
+++ kubeacct.py
@@ -11,10 +11,13 @@ group.add_argument('-m', '--memory', action='store_true')
 group.add_argument('-n', '--namespace', action='store_true')
 parser.add_argument('--all', action='store_true')
 parser.add_argument('-s', '--sortby')
+act = parser.add_mutually_exclusive_group()
+act.add_argument('-r', '--reverse', action='store_true')
 args = parser.parse_args()
 
 def wallclock(period):
-    return requests.get('https://prometheus.nautilus.optiputer.net/api/v1/query?query=sum(node_namespace_pod:kube_pod_info:) by (namespace)').json()
+        return requests.get('https://prometheus.nautilus.optiputer.net/api/v1/query?query=sum(node_namespace_pod:kube_pod_info:) by (namespace)').json()
+
 
 def gpu(period):
     return requests.get('https://prometheus.nautilus.optiputer.net/api/v1/query?query=increase(namespace_gpu_utilization[' + period + '])').json()
@@ -38,7 +41,7 @@ if __name__ == '__main__':
         header = [("namespace:", "wallclock time:"), ("------------", "---------------")]
 
         for h in header:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(h)
             print(n)
 
@@ -51,19 +54,26 @@ if __name__ == '__main__':
 
         for i in range(len(namespace)):
             if args.all:
-                name_n_val.append((namespace[i], float(values[i][1])))
+                name_n_val.append((namespace[i], round(float(values[i][1]), 1)))
             else:
                 if float(values[i][1]) != 0:
-                    name_n_val.append((namespace[i], float(values[i][1])))
+                    name_n_val.append((namespace[i], round(float(values[i][1]), 1)))
 
         def sorter(e, sortby=0):
             sortby = int(args.sortby)
             return e[sortby]
 
-        name_n_val.sort(key=sorter)
+        reverse_bool = False
+        if args.reverse:
+            reverse_bool = True
+
+        if args.sortby == "1":
+            reverse_bool = not reverse_bool
+
+        name_n_val.sort(key=sorter, reverse=reverse_bool)
 
         for name in name_n_val:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(name)
             print(n)
 
@@ -75,7 +85,7 @@ if __name__ == '__main__':
         header = [("namespace:", "gpu:"), ("------------", "------------")]
 
         for h in header:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(h)
             print(n)
 
@@ -88,19 +98,26 @@ if __name__ == '__main__':
 
         for i in range(len(g_namespace)):
             if args.all:
-                name_n_val.append((g_namespace[i], float(g_values[i][1])))
+                name_n_val.append((g_namespace[i], round(float(g_values[i][1]), 1)))
             else:
                 if float(g_values[i][1]) != 0:
-                    name_n_val.append((g_namespace[i], float(g_values[i][1])))
+                    name_n_val.append((g_namespace[i], round(float(g_values[i][1]), 1)))
 
         def sorter(e, sortby=0):
             sortby = int(args.sortby)
             return e[sortby]
 
-        name_n_val.sort(key=sorter)
+        reverse_bool = False
+        if args.reverse:
+            reverse_bool = True
+
+        if args.sortby == "1":
+            reverse_bool = not reverse_bool
+
+        name_n_val.sort(key=sorter, reverse=reverse_bool)
 
         for name in name_n_val:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(name)
             print(n)
 
@@ -111,7 +128,7 @@ if __name__ == '__main__':
         c_values = []
         header = [("namespace:", "cpu:"), ("-------------", "-------------")]
         for h in header:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(h)
             print(n)
 
@@ -124,19 +141,26 @@ if __name__ == '__main__':
 
         for i in range(len(c_namespace)):
             if args.all:
-                name_n_val.append((c_namespace[i], float(c_values[i][1])))
+                name_n_val.append((c_namespace[i], round(float(c_values[i][1]), 1)))
             else:
                 if float(c_values[i][1]) != 0:
-                    name_n_val.append((c_namespace[i], float(c_values[i][1])))
+                    name_n_val.append((c_namespace[i], round(float(c_values[i][1]), 1)))
 
         def sorter(e, sortby=0):
             sortby = int(args.sortby)
             return e[sortby]
 
-        name_n_val.sort(key=sorter)
+        reverse_bool = False
+        if args.reverse:
+            reverse_bool = True
+
+        if args.sortby == "1":
+            reverse_bool = not reverse_bool
+
+        name_n_val.sort(key=sorter, reverse=reverse_bool)
 
         for name in name_n_val:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(name)
             print(n)
 
@@ -148,7 +172,7 @@ if __name__ == '__main__':
         header = [("namespace:", "memory:"), ("------------", "------------")]
 
         for h in header:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(h)
             print(n)
 
@@ -161,19 +185,26 @@ if __name__ == '__main__':
         for i in range(len(m_namespace)):
 
             if args.all:
-                name_n_val.append((m_namespace[i], float(m_values[i][1])))
+                name_n_val.append((m_namespace[i], round(float(m_values[i][1]), 1)))
             else:
                 if float(m_values[i][1]) != 0:
-                    name_n_val.append((m_namespace[i], float(m_values[i][1])))
+                    name_n_val.append((m_namespace[i], round(float(m_values[i][1]), 1)))
 
         def sorter(e, sortby=0):
             sortby = int(args.sortby)
             return e[sortby]
 
-        name_n_val.sort(key=sorter)
+        reverse_bool = False
+        if args.reverse:
+            reverse_bool = True
+
+        if args.sortby == "1":
+            reverse_bool = not reverse_bool
+
+        name_n_val.sort(key=sorter, reverse=reverse_bool)
 
         for name in name_n_val:
-            format = "%-65s %5s"
+            format = "%-65s %20s"
             n = format%(name)
             print(n)
 
